86714e9fee7eeb72a3bef77088eb0e486b66fab5
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-09-24 17:49:40 +0200

    chore: Ensure docker-compose works

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,5 +5,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  server: {
+    port: 3000,
+    host: true,
+  },
 });
