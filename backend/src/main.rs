31a0ef1ecdead646ff6171582fbc4c1752ac2c94
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-10-10 13:39:05 +0200

    feat: Add main-frame and display 'under construction'

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,15 @@
-use rocket::{get, launch, routes};
+use rocket::launch;
 extern crate rocket;
 
 mod models;
 mod schema;
 mod system;
-
-
-#[get("/")]
-pub fn index() -> &'static str {
-  "Hello, world!"
-}
+mod root;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
       .attach(system::Cors)
-      .mount("/", routes![index])
+      .mount("/", root::routes())
       .mount("/system", system::routes())
 }
