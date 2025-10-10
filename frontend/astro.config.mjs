08bf3a3ae35d304d4c4740034c4a785fba32a779
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-10-10 10:28:35 +0200

    feat: Proxy /api calls in Vite, to work with/without Docker

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+const BACKEND_BASE_URL = (import.meta.env.MODE === 'production')
+  ? 'http://backend:8000'
+  : 'http://localhost:8000';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
@@ -10,4 +14,15 @@ export default defineConfig({
     port: 3000,
     host: true,
   },
+  vite: {
+    server: {
+      proxy: {
+        '/api': {
+          target: BACKEND_BASE_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      }
+    }
+  }
 });
