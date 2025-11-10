94e6c0de942220b63349d6d98fd0f1077aba34a6
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-11-10 09:36:43 +0100

    chore(frontend): [format] Apply prettier@3.6.2 -w

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-const BACKEND_BASE_URL = (import.meta.env.MODE === 'production')
-  ? 'http://backend:8000'
-  : 'http://localhost:8000';
+const BACKEND_BASE_URL =
+  import.meta.env.MODE === "production"
+    ? "http://backend:8000"
+    : "http://localhost:8000";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,12 +18,12 @@ export default defineConfig({
   vite: {
     server: {
       proxy: {
-        '/api': {
+        "/api": {
           target: BACKEND_BASE_URL,
           changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, ''),
+          rewrite: (path) => path.replace(/^\/api/, ""),
         },
-      }
-    }
-  }
+      },
+    },
+  },
 });
