0bd4ccc4d6e7132cbbe80aaffb612c4418f2ab07
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-11-16 17:16:57 +0100

    chore(backend): Comment out mock data down.sql (as it will be unfeasible to keep in sync with up.sql)

--- backend/migrations/2025-09-21-161517_mock_data/down.sql
+++ backend/migrations/2025-09-21-161517_mock_data/down.sql
@@ -1,22 +1,23 @@
+-- TODO:
 -- Remove mock data in reverse order of dependencies
 
 -- Remove flashcard metadata
-DELETE FROM flashcard_metadata WHERE ID_USER IN (1, 2, 3, 4);
+-- DELETE FROM flashcard_metadata WHERE ID_USER IN (1, 2, 3, 4);
 
 -- Remove flashcards
-DELETE FROM flashcard WHERE ID <= 14;
+-- DELETE FROM flashcard WHERE ID <= 14;
 
 -- Remove flashcard template plugin associations
-DELETE FROM flashcard_template_plugin WHERE template_id <= 6;
+-- DELETE FROM flashcard_template_plugin WHERE template_id <= 6;
 
 -- Remove decks
-DELETE FROM deck WHERE name IN ('Spanish Vocabulary', 'Math Formulas', 'Geography Facts', 'Programming Concepts', 'Audio Pronunciation', 'Science Diagrams');
+-- DELETE FROM deck WHERE name IN ('Spanish Vocabulary', 'Math Formulas', 'Geography Facts', 'Programming Concepts', 'Audio Pronunciation', 'Science Diagrams');
 
 -- Remove flashcard templates
-DELETE FROM flashcard_template WHERE ID <= 6;
+-- DELETE FROM flashcard_template WHERE ID <= 6;
 
 -- Remove users
-DELETE FROM "user" WHERE username IN ('john_doe', 'jane_smith', 'study_master', 'language_learner');
+-- DELETE FROM "user" WHERE username IN ('john_doe', 'jane_smith', 'study_master', 'language_learner');
 
 -- Remove plugins
-DELETE FROM plugin WHERE name IN ('Basic Card Renderer', 'Image Card Plugin', 'Math Formula Plugin', 'Audio Player Plugin');
+-- DELETE FROM plugin WHERE name IN ('Basic Card Renderer', 'Image Card Plugin', 'Math Formula Plugin', 'Audio Player Plugin');
