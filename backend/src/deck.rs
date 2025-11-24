a46c38d6d61492a14aace8231f34a9a61cce2f17
mpsanchis <33475618+mpsanchis@users.noreply.github.com>
2025-11-24 09:27:25 +0100

    chore: Cargo fmt

--- backend/src/deck.rs
+++ backend/src/deck.rs
@@ -3,7 +3,7 @@ use crate::utils::db;
 use diesel::prelude::QueryDsl;
 use diesel::{ExpressionMethods, RunQueryDsl};
 use rocket::response::status;
-use rocket::serde::{json::Json};
+use rocket::serde::json::Json;
 use rocket::{Route, get, routes as rocket_routes};
 
 use crate::models::{Deck, DeckWithCards};
@@ -23,7 +23,6 @@ fn get_decks() -> String {
     serde_json::to_string(&decks).unwrap()
 }
 
-
 #[get("/<id>")]
 fn get_deck(id: i32) -> Result<Json<DeckWithCards>, status::NotFound<String>> {
     let mut db_connection = db::get_connection().lock().unwrap();
@@ -42,13 +41,17 @@ fn get_deck(id: i32) -> Result<Json<DeckWithCards>, status::NotFound<String>> {
         .filter(card::deck_id.eq(id))
         .order_by(card::id.asc())
         .select(card::id)
-        .load(&mut *db_connection) {
-            cards
-        } else {
-            return Err(status::NotFound(format!("Cards for deck with id {} could not be loaded", id)));
-        };
+        .load(&mut *db_connection)
+    {
+        cards
+    } else {
+        return Err(status::NotFound(format!(
+            "Cards for deck with id {} could not be loaded",
+            id
+        )));
+    };
 
-    Ok(Json(DeckWithCards{deck, card_ids}))
+    Ok(Json(DeckWithCards { deck, card_ids }))
 }
 
 pub fn routes() -> Vec<Route> {
