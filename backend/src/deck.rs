a092de1936332cc4a435dcd5af3451ed1f88c7df
João Dinis Ferreira <hello@joaof.eu>
2025-12-06 14:35:20 +0100

    style: format Rust files

--- backend/src/deck.rs
+++ backend/src/deck.rs
@@ -1,8 +1,8 @@
 use crate::schema::{card, deck};
 use crate::utils::db;
+use diesel::Connection;
 use diesel::prelude::QueryDsl;
 use diesel::{ExpressionMethods, RunQueryDsl};
-use diesel::Connection;
 use rocket::http::Status;
 use rocket::response::status;
 use rocket::serde::json::Json;
@@ -56,7 +56,6 @@ fn get_deck(id: i32) -> Result<Json<DeckWithCards>, status::NotFound<String>> {
     Ok(Json(DeckWithCards { deck, card_ids }))
 }
 
-
 #[delete("/<id>")]
 fn delete_deck(id: i32) -> Result<status::NoContent, status::Custom<String>> {
     let mut db_connection = db::get_connection().lock().unwrap();
@@ -64,46 +63,45 @@ fn delete_deck(id: i32) -> Result<status::NoContent, status::Custom<String>> {
     rocket::info!("Deleting deck {id}");
 
     // Use a transaction to ensure atomicity
-    db_connection.transaction::<_, diesel::result::Error, _>(|conn| {
-        // First, delete all cards associated with this deck
-        let num_cards = diesel::delete(card::table.filter(card::deck_id.eq(id)))
-            .execute(conn)?;
+    db_connection
+        .transaction::<_, diesel::result::Error, _>(|conn| {
+            // First, delete all cards associated with this deck
+            let num_cards =
+                diesel::delete(card::table.filter(card::deck_id.eq(id))).execute(conn)?;
 
-        rocket::info!("Deleted {} cards from deck {}", num_cards, id);
+            rocket::info!("Deleted {} cards from deck {}", num_cards, id);
 
-        // Then delete the deck itself
-        let num_decks = diesel::delete(deck::table.filter(deck::id.eq(id)))
-            .execute(conn)?;
+            // Then delete the deck itself
+            let num_decks = diesel::delete(deck::table.filter(deck::id.eq(id))).execute(conn)?;
 
-        if num_decks == 0 {
-            return Err(diesel::result::Error::NotFound);
-        }
+            if num_decks == 0 {
+                return Err(diesel::result::Error::NotFound);
+            }
 
-        Ok(())
-    })
-    .map(|_| {
-        rocket::info!(
-            "Deleted deck {id}. Number of decks left: {}",
-            deck::table
-                .count()
-                .get_result::<i64>(&mut *db_connection)
-                .unwrap()
-        );
-        status::NoContent
-    })
-    .map_err(|e| match e {
-        diesel::result::Error::NotFound => status::Custom(
-            Status::NotFound,
-            format!("Deck with id {} not found", id)
-        ),
-        _ => {
-            eprintln!("Database error deleting deck {}: {:?}", id, e);
-            status::Custom(
-                Status::InternalServerError,
-                "Failed to delete deck".to_string(),
-            )
-        }
-    })
+            Ok(())
+        })
+        .map(|_| {
+            rocket::info!(
+                "Deleted deck {id}. Number of decks left: {}",
+                deck::table
+                    .count()
+                    .get_result::<i64>(&mut *db_connection)
+                    .unwrap()
+            );
+            status::NoContent
+        })
+        .map_err(|e| match e {
+            diesel::result::Error::NotFound => {
+                status::Custom(Status::NotFound, format!("Deck with id {} not found", id))
+            }
+            _ => {
+                eprintln!("Database error deleting deck {}: {:?}", id, e);
+                status::Custom(
+                    Status::InternalServerError,
+                    "Failed to delete deck".to_string(),
+                )
+            }
+        })
 }
 
 pub fn routes() -> Vec<Route> {
